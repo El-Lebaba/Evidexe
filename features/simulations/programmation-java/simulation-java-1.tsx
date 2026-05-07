4468e0712c6535d3529cf8c6c32c3ef3f6dbffb1
Arold 9555 <Arold9555@gmail.com>
2026-05-07 17:53:49 -0400

    naming tri files,

--- features/simulations/programmation-java/simulation-java-1.tsx
+++
@@ -1,6 +0,0 @@
-import { EcranSimulationLigne } from '@/features/simulations/core/ecran-simulation-ligne';
-
-export function SimulationJava1() {
-  return <EcranSimulationLigne titre="Java 1" domaine="programmation-java"/>;
-}
-
